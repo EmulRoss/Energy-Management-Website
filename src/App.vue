317a2e3dc7bb0d98cb03d8c307082a36693b0b3b
EmulRoss <emul.ross@gmail.com>
2018-10-22 00:12:42 +0200

    Interface Update
    Changed navigation bar
    Changed some styling
    Added GeoJSON data

--- src/App.vue
+++ src/App.vue
@@ -4,38 +4,34 @@
 
       <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+      <b-navbar-brand href="#">Wits Energy Management</b-navbar-brand>
 
       <b-collapse is-nav id="nav_collapse">
 
-        <b-navbar-nav>
-          <b-nav-item href="#">Link</b-nav-item>
-          <b-nav-item href="#" disabled>Disabled</b-nav-item>
-        </b-navbar-nav>
+        <b-nav-form>
+          <b-button-group>
+            <b-button :pressed= "true">Daily</b-button>
+            <b-button :pressed= "false">Monthly</b-button>
+            <b-button :pressed= "false">Yearly</b-button>
+          </b-button-group>
+        </b-nav-form>
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
 
           <b-nav-form>
-            <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+            <b-button-group>
+              <b-button :pressed= "false">Actuals</b-button>
+              <b-button :pressed= "true">Deviation</b-button>
+            </b-button-group>
           </b-nav-form>
 
-          <b-nav-item-dropdown text="Lang" right>
-            <b-dropdown-item href="#">EN</b-dropdown-item>
-            <b-dropdown-item href="#">ES</b-dropdown-item>
-            <b-dropdown-item href="#">RU</b-dropdown-item>
-            <b-dropdown-item href="#">FA</b-dropdown-item>
+
+          <b-nav-item-dropdown text="Entity Selection" right>
+            <b-dropdown-item href="#">Braamfontein Campus</b-dropdown-item>
           </b-nav-item-dropdown>
 
-          <b-nav-item-dropdown right>
-            <!-- Using button-content slot -->
-            <template slot="button-content">
-              <em>User</em>
-            </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Signout</b-dropdown-item>
-          </b-nav-item-dropdown>
+          
         </b-navbar-nav>
 
       </b-collapse>
